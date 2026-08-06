ffa012a9f53d4f2439468d3e6189bbdeea672957
jeffeped <291378025+jeffeped@users.noreply.github.com>
2026-08-06 01:06:56 -0400

    Corrige registro semanal dos internos

--- internos/sw.js
+++ internos/sw.js
@@ -1,4 +1,4 @@
-const CACHE='internoped-uea-v1';
+const CACHE='internoped-uea-v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
