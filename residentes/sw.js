088e27a48969e8354628732816adef1f6aa033f0
jeffeped <291378025+jeffeped@users.noreply.github.com>
2026-08-28 20:56:16 -0400

    Corrigir sincronização de cadastro de residentes

--- residentes/sw.js
+++ residentes/sw.js
@@ -1,4 +1,4 @@
-const CACHE='residped-uea-v4';
+const CACHE='residped-uea-v5';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
