12af3b0569a48129279e15ac16098a79aa43ec20
jeffeped <291378025+jeffeped@users.noreply.github.com>
2026-09-01 18:25:00 -0400

    Permitir configurar local por módulo no portal

--- portal-preceptor/sw.js
+++ portal-preceptor/sw.js
@@ -1,4 +1,4 @@
-const CACHE='portal-preceptor-v5';
+const CACHE='portal-preceptor-v6';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
